9380638cd927637d57e6520b94b96b3f7ed2e0e5
Sande Gilda <sgilda@redhat.com>
2015-06-16 14:05:34 -0400

    Update script to replace doc links after building the guides

--- scripts/windupDocUpdate.sh
+++ scripts/windupDocUpdate.sh
@@ -125,9 +125,6 @@ find . -name '*.adoc' -print | xargs sed -i 's/:ProductHomeVar: WINDUP_HOME//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocHomeVar: WINDUP_DOCUMENTATION_HOME//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductSrcHomeVar: WINDUP_SOURCE_HOME//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductReleaseVar: WINDUP_RELEASE//g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocUserGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupUserGuide.html//g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocRulesGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupRulesDevelopmentGuide.html//g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocCoreGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupCoreDevelopmentGuide.html//g'
 
 ##find . -name '*.adoc' -print | xargs sed -i 's/{ProductName}/Windup/g'
 ##find . -name '*.adoc' -print | xargs sed -i 's/{ProductVersion}/2.2.0-Final/g'
@@ -183,6 +180,13 @@ echo ""
 echo "*******************************************"
 echo "* Build for DocStage"
 echo "*******************************************"
+echo "*******************************************"
+echo "* Replace documentation links... "
+echo "*******************************************"
+
+find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocUserGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupUserGuide.html//g'
+find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocRulesGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupRulesDevelopmentGuide.html//g'
+find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocCoreGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupCoreDevelopmentGuide.html//g'
 
 echo ""
 echo "*******************************************"
