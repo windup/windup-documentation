4a0cdf1ce3db079b3afd3bcf71c0360625ac68f1
Sande Gilda <sgilda@redhat.com>
2015-09-03 11:27:43 -0400

    Updates for Windup 2.4.0.Final

--- scripts/windupDocUpdate.sh
+++ scripts/windupDocUpdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ "$1" == "" ] || [ "$2" == "" ]; then 
+if [ "$1" == "?" ]  || [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "" ] || [ "$2" == "" ]; then 
 
+    echo "Run this script from the root of the windup-documentation directory." 
     echo "You must pass the path to the windup.wiki and windup-documentation GitHub directories on the command line." 
     echo "For example:"
     echo "    scripts/windupDocUpdate.sh ~/GitRepos/windup.wiki ~/GitRepos/windup-documentation/"
@@ -13,6 +14,9 @@ fi
 echo "*******************************************"
 echo "* Fetching the latest pages from the Windup Wiki..."
 echo "*******************************************"
+PRODUCT_DOC_VERSION=version-2.4
+OLD_PRODUCT_VERSION=2.2.0-Final
+PRODUCT_VERSION=2.4.0-Final
 cd $1
 pwd
 
@@ -119,20 +123,22 @@ echo ""
 cd docs/
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductName: Windup//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductShortName: Windup//g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductVersion: 2.2.0-Final//g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDistribution: windup-distribution-2.2.0-Final//g'
+find . -name '*.adoc' -print | xargs sed -i 's/:ProductVersion: '$OLD_PRODUCT_VERSION'//g'
+find . -name '*.adoc' -print | xargs sed -i 's/:ProductDistribution: windup-distribution-'$OLD_PRODUCT_VERSION'//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductHomeVar: WINDUP_HOME//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocHomeVar: WINDUP_DOCUMENTATION_HOME//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductSrcHomeVar: WINDUP_SOURCE_HOME//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductReleaseVar: WINDUP_RELEASE//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocUserGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupUserGuide.html//g'
 find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocRulesGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupRulesDevelopmentGuide.html//g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocCoreGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupCoreDevelopmentGuide.html//g'
+
+## Windup Core Development Guide has NOT yet been published!
+##find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocCoreGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupCoreDevelopmentGuide.html//g'
 
 
 ##find . -name '*.adoc' -print | xargs sed -i 's/{ProductName}/Windup/g'
-##find . -name '*.adoc' -print | xargs sed -i 's/{ProductVersion}/2.2.0-Final/g'
-##find . -name '*.adoc' -print | xargs sed -i 's/{ProductDistribution}/windup-distribution-2.2.0-Final/g'
+##find . -name '*.adoc' -print | xargs sed -i 's/{ProductVersion}/'$PRODUCT_VERSION'/g'
+##find . -name '*.adoc' -print | xargs sed -i 's/{ProductDistribution}/windup-distribution-'$PRODUCT_VERSION'/g'
 ##find . -name '*.adoc' -print | xargs sed -i 's/{ProductHomeVar}/WINDUP_HOME/g'
 ##find . -name '*.adoc' -print | xargs sed -i 's/{ProductDocHomeVar}/WINDUP_DOCUMENTATION_HOME/g'
 ##find . -name '*.adoc' -print | xargs sed -i 's/{ProductSrcHomeVar}/WINDUP_SOURCE_HOME/g'
@@ -143,8 +149,8 @@ cp Windup-User-Guide.adoc docs/
 cp Windup-Rules-Development-Guide.adoc docs/
 cp Windup-Core-Development-Guide.adoc docs/
 
-cp Windup-User-Guide-NO-TOC.adoc docs/
-cp Windup-Rules-Development-Guide-NO-TOC.adoc docs/
+#cp Windup-User-Guide-NO-TOC.adoc docs/
+#cp Windup-Rules-Development-Guide-NO-TOC.adoc docs/
 cp Windup-Core-Development-Guide-NO-TOC.adoc docs/
 
 echo "Removal of variables is complete."
@@ -156,7 +162,7 @@ echo "*******************************************"
 asciidoctor -t -dbook -a toc -o html/WindupUserGuide.html docs/Windup-User-Guide.adoc
 asciidoctor -t -dbook -o html/WindupUserGuide-NO-TOC.html docs/Windup-User-Guide-NO-TOC.adoc
 wkhtmltopdf --page-size Letter html/WindupUserGuide-NO-TOC.html pdf/WindupUserGuide.pdf
-#echo "User Guide is located at: " file://$2/html/WindupUserGuide.html
+#echo "User Guide is located at: " file://$2/pdf/WindupUserGuide.pdf
 echo ""
 
 
@@ -167,7 +173,7 @@ asciidoctor -t -dbook -a toc -o html/WindupRulesDevelopmentGuide.html docs/Windu
 asciidoctor -t -dbook -o html/WindupRulesDevelopmentGuide-NO-TOC.html docs/Windup-Rules-Development-Guide.adoc
 wkhtmltopdf --page-size Letter html/WindupRulesDevelopmentGuide-NO-TOC.html pdf/WindupRulesDevelopmentGuide.pdf
 ## Build docbook xml: asciidoctor -b docbook docs/Windup-Rules-Development-Guide.adoc
-## echo "Guide is located at: " file://$2/html/WindupRulesDevelopmentGuide.html
+## echo "Guide is located at: " file://$2/pdf/WindupRulesDevelopmentGuide.pdf
 echo ""
 echo "*******************************************"
 echo "* Building the Windup Core Development Guide..."
@@ -176,7 +182,7 @@ asciidoctor -t -dbook -a toc -o html/WindupCoreDevelopmentGuide.html docs/Windup
 asciidoctor -t -dbook -o html/WindupCoreDevelopmentGuide-NO-TOC.html docs/Windup-Core-Development-Guide-NO-TOC.adoc
 wkhtmltopdf --page-size Letter html/WindupCoreDevelopmentGuide-NO-TOC.html pdf/WindupCoreDevelopmentGuide.pdf
 ## Build docbook xml: asciidoctor -b docbook docs/Windup-Core-Development-Guide.adoc
-## echo "Guide is located at: " file://$2/html/WindupCoreDevelopmentGuide.html
+## echo "Guide is located at: " file://$2/pdf/WindupCoreDevelopmentGuide.pdf
 echo ""
 
 echo ""
@@ -189,9 +195,10 @@ echo "*******************************************"
 
 cd docs/
 
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocUserGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupUserGuide.html/:ProductDocUserGuideURL: https:\/\/access.redhat.com\/documentation\/en\/red-hat-jboss-migration-toolkit\/version-2.3\/windup-user-guide/g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocRulesGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupRulesDevelopmentGuide.html/:ProductDocRulesGuideURL: https:\/\/access.redhat.com\/documentation\/en\/red-hat-jboss-migration-toolkit\/version-2.3\/windup-rules-development-guide/g'
-find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocCoreGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupCoreDevelopmentGuide.html/:ProductDocCoreGuideURL: https:\/\/access.redhat.com\/documentation\/en\/red-hat-jboss-migration-toolkit\/version-2.3\/windup-core-development-guide/g'
+find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocUserGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupUserGuide.html/:ProductDocUserGuideURL: https:\/\/access.redhat.com\/documentation\/en\/red-hat-jboss-migration-toolkit\/'$PRODUCT_DOC_VERSION'\/windup-user-guide/g'
+find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocRulesGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupRulesDevelopmentGuide.html/:ProductDocRulesGuideURL: https:\/\/access.redhat.com\/documentation\/en\/red-hat-jboss-migration-toolkit\/'$PRODUCT_DOC_VERSION'\/windup-rules-development-guide/g'
+## Windup Core Development Guid has not been published to the portal
+## find . -name '*.adoc' -print | xargs sed -i 's/:ProductDocCoreGuideURL: http:\/\/windup.github.io\/windup\/docs\/latest\/html\/WindupCoreDevelopmentGuide.html/:ProductDocCoreGuideURL: https:\/\/access.redhat.com\/documentation\/en\/red-hat-jboss-migration-toolkit\/'$PRODUCT_DOC_VERSION'\/windup-core-development-guide/g'
 
 cd ../
 
@@ -204,11 +211,12 @@ echo "*******************************************"
 
 ccutil compile --lang en_US --main-file ~/GitRepos/windup-documentation/docs/Windup-User-Guide.adoc
 ## Save the documentation so it is not lost when building another doc: cp -r build/ build-windup-user-guide/ 
-cp -r docs/images/ build/tmp/en_US/html-single/
-cp -r docs/images/ build/en_US/
+cp -r docs/images/ build/tmp/en-US/html-single/
+cp -r docs/images/ build/en-US/
+ls build/tmp/en-US/html-single/
 rm -r build-user-guide
 mv build/ build-user-guide
-###echo "DocBook User Guide is located at: " file://$2/build-user-guide/tmp/en_US/html-single/index.html
+###echo "DocBook User Guide is located at: " file://$2/build-user-guide/tmp/en-US/html-single/index.html
 echo ""
 
 echo ""
@@ -216,11 +224,11 @@ echo "***********************************************"
 echo "* Building the Windup Rules Development Guide *"
 echo "***********************************************"
 ccutil compile --lang en_US --main-file ~/GitRepos/windup-documentation/docs/Windup-Rules-Development-Guide.adoc
-cp -r docs/images/ build/tmp/en_US/html-single/
-cp -r docs/images/ build/en_US/
+cp -r docs/images/ build/tmp/en-US/html-single/
+cp -r docs/images/ build/en-US/
 rm -r build-rules-guide
 mv build/ build-rules-guide
-###echo "DocBook Guide is located at: " file://$2/build-rules-guide/tmp/en_US/html-single/index.html
+###echo "DocBook Guide is located at: " file://$2/build-rules-guide/tmp/en-US/html-single/index.html
 echo ""
 
 echo ""
@@ -228,26 +236,26 @@ echo "***********************************************"
 echo "* Building the Windup Core Development Guide  *"
 echo "***********************************************"
 ccutil compile --lang en_US --main-file ~/GitRepos/windup-documentation/docs/Windup-Core-Development-Guide.adoc
-cp -r docs/images/ build/tmp/en_US/html-single/
-cp -r docs/images/ build/en_US/
+cp -r docs/images/ build/tmp/en-US/html-single/
+cp -r docs/images/ build/en-US/
 rm -r build-core-guide
 mv build/ build-core-guide
-###echo "DocBook Guide is located at: " file://$2/build-core-guide/tmp/en_US/html-single/index.html
+###echo "DocBook Guide is located at: " file://$2/build-core-guide/tmp/en-US/html-single/index.html
 echo ""
 
 echo "User Guide is located at: " file://$2/html/WindupUserGuide.html
 echo "User Guide (PDF) is located at: " file://$2/pdf/WindupUserGuide.pdf
-echo "User Guide (DocBook) is located at: " file://$2/build-user-guide/tmp/en_US/html-single/index.html
+echo "User Guide (DocBook) is located at: " file://$2/build-user-guide/tmp/en-US/html-single/index.html
 echo ""
 
 echo "Rules Guide is located at: " file://$2/html/WindupRulesDevelopmentGuide.html
 echo "Rules Guide (PDF) is located at: " file://$2/pdf/WindupRulesDevelopmentGuide.pdf
-echo "Rules Guide (DocBook) is located at: " file://$2/build-rules-guide/tmp/en_US/html-single/index.html
+echo "Rules Guide (DocBook) is located at: " file://$2/build-rules-guide/tmp/en-US/html-single/index.html
 echo ""
 
 echo "Core Guide is located at: " file://$2/html/WindupCoreDevelopmentGuide.html
 echo "Core Guide (PDF) is located at: " file://$2/pdf/WindupCoreDevelopmentGuide.pdf
-echo "Core Guide (DocBook) is located at: " file://$2/build-core-guide/tmp/en_US/html-single/index.html
+echo "Core Guide (DocBook) is located at: " file://$2/build-core-guide/tmp/en-US/html-single/index.html
 echo ""
 echo "*******************************************"
 echo "Manually verify the guides and use Maven to check in the new files"
