0553a97e308e8a22c56581b87ead94c3c709a3a7
Avital Pinnick <apinnick@redhat.com>
2021-11-23 12:47:20 +0200

    update

--- build.sh
+++ build.sh
@@ -10,14 +10,17 @@ done
 echo "HTML files built"
 
 rm index.adoc > /dev/null 2>&1
+rm index.md > /dev/null 2>&1
 
-cat << EOF > index.adoc
-= Windup documentation
+cat << EOF > index.md
+---
+layout: default
+---
 
 EOF
 
 for f in $FILES; do
-  echo -e "* link:$BASE_URL/$(dirname $f)/master.html[$(dirname $f | sed 's/docs\///g; s/_/ /g; s/-/ /g' )]" >> index.adoc
+  echo -e "* link:$BASE_URL/$(dirname $f)/master.html[$(dirname $f | sed 's/docs\///g; s/_/ /g; s/-/ /g' )]" >> index.md
 done
 
-echo "index.adoc built"
+echo "index.md built"
